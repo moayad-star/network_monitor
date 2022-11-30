00fe747ba124a73cbff7f607e658b2a2236ed604
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2022-11-30 20:21:39 +0300

    c++

---
+++ network_scanning.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "The program is under \ndevelopment for the purpose of supporting \nC++ programming language\n";
+    std::cout << "Thanks for your support";
+    return 0;
+}

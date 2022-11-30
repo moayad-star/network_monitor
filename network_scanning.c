d2c63982ad122671e817bc823624d12e1a58a5cf
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2022-11-30 20:14:17 +0300

    c language

--- network_scanning.c
+++ network_scanning.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 int man(){
   printf("The program is under development \nfor the purpose of supporting \nthe c programming language");
   printf("\n\nthanks for your support");

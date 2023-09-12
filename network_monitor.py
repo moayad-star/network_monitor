f8ffea73f10e6e9b951c93662c27d61ab7c5e428
Moayad Ahmed <60769512+moayad-star@users.noreply.github.com>
2023-09-12 06:23:37 +0300

    Python

--- network_monitor.py
+++ network_monitor.py
@@ -1,39 +1,31 @@
-try:
-    # Color
-    R = "\033[;31;m" #Red 
-    G = "\033[;32;m" #Green
-    B = "\033[;34;m" #Blue
-    Y = "\033[;33;m" #Yellow
-    W = "\033[;0;m"  #White
+# Color
+R = "\033[;31;m" #Red 
+G = "\033[;32;m" #Green
+B = "\033[;34;m" #Blue
+Y = "\033[;33;m" #Yellow
+W = "\033[;0;m"  #White
     
-    # Module
-    from os import popen as UX # os >> popen
-    from time import strftime,sleep # time >> strftime,sleep
-    from ipaddress import IPv4Address
-    try:
-        import requests
-    except ModuleNotFoundError:
-        print(f"[{R}WARNING{W}]Download Module '{G}requests{W}' ....")
-        UX("pip install requests")
-        import requests
-        print(f"{W}[{G}INFO{W}] module '{G}requsts{W}' is installed")
-    try:
-        import readline
-    except ModuleNotFoundError:
-        print(f"[{R}WARNING{W}]Download Module '{G}readline{W}' ....") 
-        UX("pkg i readline*")
-        import readline
-        print(f"{W}[{G}INFO{W}] module '{G}readline{W}' is installed")
-    try:
-        import nmap
-    except ModuleNotFoundError:
-        print(f"[{R}WARNING{W}]Download Module '{G}requests{W}' ....")
-        UX("pip install python-nmap")
-        import nmap
-        print(f"{W}[{G}INFO{W}] module '{G}nmap{W}' is installed")
+# Module
+from os import popen as UX # os >> popen
+from time import strftime,sleep # time >> strftime,sleep
+from ipaddress import IPv4Address
+try:
+    import requests
+except ModuleNotFoundError:
+    print(f"[{R}WARNING{W}]Download Module '{G}requests{W}' ....")
+    UX("pip install requests")
+    import requests
+    print(f"{W}[{G}INFO{W}] module '{G}requsts{W}' is installed")
+try:
+    import nmap
+except ModuleNotFoundError:
+    print(f"[{R}WARNING{W}]Download Module '{G}requests{W}' ....")
+    UX("pip install python-nmap")
+    import nmap
+    print(f"{W}[{G}INFO{W}] module '{G}nmap{W}' is installed")
 
-    #get IPs
-    try:
+def main():
+    try: #get IPs
         your_ip = str(UX("ifconfig | grep 'broadcast'").read()).split()[1]
         original_ips = [your_ip]
         common_subnet = ".".join(original_ips[0].split(".")[:3]) + ".0"
@@ -56,9 +48,14 @@ try:
             if ip not in nm.all_hosts():
                 list_ips.remove(ip)
                 print(f"{W}[{B}{strftime('%H:%M:%S')}{W}] [{R}DISCONNECT{W}] [{B}Total:{G}{len(list_ips)}{W}]  >>> {R}{ip}{W}")
-except (KeyboardInterrupt,EOFError):
-    print(f"""
+        sleep(10)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt,EOFError):
+        print(f"""
     {Y} User:{R} CTRL + C \n
     {Y} User stop the operation \n 
     {R} Exit ...
-    """)
+        """)
